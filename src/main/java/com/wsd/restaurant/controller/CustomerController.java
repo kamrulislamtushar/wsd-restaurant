68fca725ee75bf9ccbb7399e5d21487572896115
Kamrul Islam Tushar <kamrul.islam@bracits.com>
2024-01-31 06:18:36 +0600

    Get all customers API implement

---
+++ src/main/java/com/wsd/restaurant/controller/CustomerController.java
@@ -0,0 +1,60 @@
+package com.wsd.restaurant.controller;
+
+import com.wsd.restaurant.dto.OrderDTO;
+import com.wsd.restaurant.dto.UserDTO;
+import com.wsd.restaurant.dto.response.PageInfo;
+import com.wsd.restaurant.dto.response.Response;
+import com.wsd.restaurant.service.UserService;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+@RestController
+@RequestMapping("/api/customers")
+public class CustomerController {
+
+    private static final List<String> ALLOWED_ORDERED_PROPERTIES = Collections.unmodifiableList(
+        Arrays.asList("id", "login", "firstName", "lastName", "email", "activated", "langKey")
+    );
+
+    private final Logger log = LoggerFactory.getLogger(CustomerController.class);
+
+    private final UserService userService;
+
+    public CustomerController(UserService userService) {
+        this.userService = userService;
+    }
+
+    /**
+     * {@code GET /users} : get all users with only public information - calling this method is allowed for anyone.
+     *
+     * @param pageable the pagination information.
+     * @return the {@link ResponseEntity} with status {@code 200 (OK)} and with body all users.
+     */
+    @GetMapping("")
+    public ResponseEntity<Response<List<UserDTO>>> getAllPublicUsers(Pageable pageable) {
+        log.debug("REST request to get all public User names");
+        if (!onlyContainsAllowedProperties(pageable)) {
+            return ResponseEntity.badRequest().build();
+        }
+        final Page<UserDTO> page = userService.getAllPublicUsers(pageable);
+        return  ResponseEntity.ok().body(new Response(page.getContent(), PageInfo.of(page)));
+    }
+
+    private boolean onlyContainsAllowedProperties(Pageable pageable) {
+        return pageable.getSort().stream().map(Sort.Order::getProperty).allMatch(ALLOWED_ORDERED_PROPERTIES::contains);
+    }
+
+}
